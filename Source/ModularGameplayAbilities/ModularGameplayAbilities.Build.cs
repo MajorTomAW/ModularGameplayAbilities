a2712cec8de20c54a13a51565c81e0370190c264
MajorTomAW <tom.wrnr.private@gmail.com>
2025-01-02 14:04:18 +0100

    updated dependencies

--- Source/ModularGameplayAbilities/ModularGameplayAbilities.Build.cs
+++ Source/ModularGameplayAbilities/ModularGameplayAbilities.Build.cs
@@ -6,48 +6,20 @@ public class ModularGameplayAbilities : ModuleRules
 {
 	public ModularGameplayAbilities(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
+		PublicDependencyModuleNames.AddRange(new[] 
+		{ 
+			"Core",
+			"GameFeatures",
+			"GameplayAbilities",
+			"GameplayTasks",
+			"GameplayTags",
+		});
 			
 		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
-				// ... add other public dependencies that you statically link with here ...
-			}
-			);
-			
-		
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"CoreUObject",
-				"Engine",
-				"Slate",
-				"SlateCore",
-				// ... add private dependencies that you statically link with here ...	
-			}
-			);
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
+		PrivateDependencyModuleNames.AddRange(new[]
+		{ 
+			"CoreUObject", 
+			"Engine",
+		});
 	}
 }
