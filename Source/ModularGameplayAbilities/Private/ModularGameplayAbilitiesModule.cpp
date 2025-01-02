b1abb822193a39733bd6726bec0c066df2fd5933
MajorTomAW <tom.wrnr.private@gmail.com>
2025-01-02 14:04:08 +0100

    Renamed the module to module

--- Source/ModularGameplayAbilities/Private/ModularGameplayAbilitiesModule.cpp
+++ Source/ModularGameplayAbilities/Private/ModularGameplayAbilitiesModule.cpp
@@ -1,6 +1,6 @@
 ﻿// Copyright Epic Games, Inc. All Rights Reserved.
 
-#include "ModularGameplayAbilities.h"
+#include "ModularGameplayAbilitiesModule.h"
 
 #define LOCTEXT_NAMESPACE "FModularGameplayAbilitiesModule"
 
