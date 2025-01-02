b7a83624d0949e44a478ebd7b72c08384ea31ad3
MajorTomAW <tom.wrnr.private@gmail.com>
2025-01-02 14:03:27 +0100

    Ability Subsystem

---
+++ Source/ModularGameplayAbilities/Private/ModularAbilitySubsystem.cpp
@@ -0,0 +1,214 @@
+// Copyright © 2024 Playton. All Rights Reserved.
+
+
+#include "ModularAbilitySubsystem.h"
+
+#include "AbilitySystemLog.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(ModularAbilitySubsystem)
+
+//////////////////////////////////////////////////////////////////////////
+/// FGloballyAppliedAbilities
+
+void FGloballyAppliedAbilities::AddToAbilitySystem(
+	TSubclassOf<UGameplayAbility> Ability, UModularAbilitySystemComponent* AbilitySystem)
+{
+	if (void* Handle = Handles.Find(AbilitySystem))
+	{
+		RemoveFromAbilitySystem(AbilitySystem);
+	}
+
+	UGameplayAbility* CDO = Ability->GetDefaultObject<UGameplayAbility>();
+	const FGameplayAbilitySpec Spec(CDO);
+	const FGameplayAbilitySpecHandle Handle = AbilitySystem->GiveAbility(Spec);
+	Handles.Add(AbilitySystem, Handle);
+}
+
+void FGloballyAppliedAbilities::RemoveFromAbilitySystem(UModularAbilitySystemComponent* AbilitySystem)
+{
+	if (const FGameplayAbilitySpecHandle* Handle = Handles.Find(AbilitySystem))
+	{
+		AbilitySystem->ClearAbility(*Handle);
+		Handles.Remove(AbilitySystem);
+	}
+}
+
+void FGloballyAppliedAbilities::RemoveFromAll()
+{
+	for (auto& KVP : Handles)
+	{
+		if (KVP.Key != nullptr)
+		{
+			KVP.Key->ClearAbility(KVP.Value);
+		}
+	}
+
+	Handles.Empty();
+}
+
+//////////////////////////////////////////////////////////////////////////
+/// FGloballyAppliedEffects
+
+void FGloballyAppliedEffects::AddToAbilitySystem(
+	TSubclassOf<UGameplayEffect> Effect, UModularAbilitySystemComponent* AbilitySystem)
+{
+	if (void* Handle = Handles.Find(AbilitySystem))
+	{
+		RemoveFromAbilitySystem(AbilitySystem);
+	}
+
+	const UGameplayEffect* CDO = Effect->GetDefaultObject<UGameplayEffect>();
+	const FActiveGameplayEffectHandle Handle = AbilitySystem->ApplyGameplayEffectToSelf(CDO, 1.f, AbilitySystem->MakeEffectContext());
+	Handles.Add(AbilitySystem, Handle);
+}
+
+void FGloballyAppliedEffects::RemoveFromAbilitySystem(UModularAbilitySystemComponent* AbilitySystem)
+{
+	if (const FActiveGameplayEffectHandle* Handle = Handles.Find(AbilitySystem))
+	{
+		AbilitySystem->RemoveActiveGameplayEffect(*Handle);
+		Handles.Remove(AbilitySystem);
+	}
+}
+
+void FGloballyAppliedEffects::RemoveFromAll()
+{
+	for (auto& KVP : Handles)
+	{
+		if (KVP.Key != nullptr)
+		{
+			KVP.Key->RemoveActiveGameplayEffect(KVP.Value);
+		}
+	}
+
+	Handles.Empty();
+}
+
+//////////////////////////////////////////////////////////////////////////
+/// UModularAbilitySubsystem
+
+UModularAbilitySubsystem::UModularAbilitySubsystem()
+{
+}
+
+bool UModularAbilitySubsystem::ShouldCreateSubsystem(UObject* Outer) const
+{
+	if (!Super::ShouldCreateSubsystem(Outer))
+	{
+		return false;
+	}
+
+	// We don't want to create the subsystem if there are any derived classes
+	// This is to prevent multiple subsystems from being created
+	TArray<UClass*> DerivedClasses;
+	GetDerivedClasses(GetClass(), DerivedClasses, false);
+	return DerivedClasses.Num() == 0;
+}
+
+void UModularAbilitySubsystem::RegisterAbilitySystem(UModularAbilitySystemComponent* AbilitySystem, bool bGrantPendingAbilities, bool bGrantPendingEffects)
+{
+	check(AbilitySystem);
+
+	if (bGrantPendingAbilities)
+	{
+		for (auto& Entry : GloballyAppliedAbilities)
+		{
+			Entry.Value.AddToAbilitySystem(Entry.Key, AbilitySystem);
+		}
+	}
+
+	if (bGrantPendingEffects)
+	{
+		for (auto& Entry : GloballyAppliedEffects)
+		{
+			Entry.Value.AddToAbilitySystem(Entry.Key, AbilitySystem);
+		}
+	}
+
+	RegisteredAbilitySystems.AddUnique(AbilitySystem);
+}
+
+void UModularAbilitySubsystem::UnregisterAbilitySystem(UModularAbilitySystemComponent* AbilitySystem)
+{
+	check(AbilitySystem);
+
+	for (auto& Entry : GloballyAppliedAbilities)
+	{
+		Entry.Value.RemoveFromAbilitySystem(AbilitySystem);
+	}
+
+	for (auto& Entry : GloballyAppliedEffects)
+	{
+		Entry.Value.RemoveFromAbilitySystem(AbilitySystem);
+	}
+
+	RegisteredAbilitySystems.Remove(AbilitySystem);
+}
+
+void UModularAbilitySubsystem::ApplyAbilityToAll(TSubclassOf<UGameplayAbility> Ability)
+{
+	if (Ability.Get() == nullptr)
+	{
+		ABILITY_LOG(Error, TEXT("Attempted to apply a null ability to all ability systems."));
+		return;
+	}
+
+	if (!GloballyAppliedAbilities.Contains(Ability))
+	{
+		FGloballyAppliedAbilities& Entry = GloballyAppliedAbilities.Add(Ability);
+		for (UModularAbilitySystemComponent* AbilitySystem : RegisteredAbilitySystems)
+		{
+			Entry.AddToAbilitySystem(Ability, AbilitySystem);
+		}
+	}
+}
+
+void UModularAbilitySubsystem::ApplyEffectToAll(TSubclassOf<UGameplayEffect> Effect)
+{
+	if (Effect.Get() == nullptr)
+	{
+		ABILITY_LOG(Error, TEXT("Attempted to apply a null effect to all ability systems."));
+		return;
+	}
+
+	if (!GloballyAppliedEffects.Contains(Effect))
+	{
+		FGloballyAppliedEffects& Entry = GloballyAppliedEffects.Add(Effect);
+		for (UModularAbilitySystemComponent* AbilitySystem : RegisteredAbilitySystems)
+		{
+			Entry.AddToAbilitySystem(Effect, AbilitySystem);
+		}
+	}
+}
+
+void UModularAbilitySubsystem::RemoveAbilityFromAll(TSubclassOf<UGameplayAbility> Ability)
+{
+	if (Ability.Get() == nullptr)
+	{
+		ABILITY_LOG(Error, TEXT("Attempted to remove a null ability from all ability systems."));
+		return;
+	}
+
+	if (GloballyAppliedAbilities.Contains(Ability))
+	{
+		FGloballyAppliedAbilities& Entry = GloballyAppliedAbilities[Ability];
+		Entry.RemoveFromAll();
+		GloballyAppliedAbilities.Remove(Ability);
+	}
+}
+
+void UModularAbilitySubsystem::RemoveEffectFromAll(TSubclassOf<UGameplayEffect> Effect)
+{
+	if (Effect.Get() == nullptr)
+	{
+		ABILITY_LOG(Error, TEXT("Attempted to remove a null effect from all ability systems."));
+		return;
+	}
+
+	if (GloballyAppliedEffects.Contains(Effect))
+	{
+		FGloballyAppliedEffects& Entry = GloballyAppliedEffects[Effect];
+		Entry.RemoveFromAll();
+		GloballyAppliedEffects.Remove(Effect);
+	}
+}
---
+++ Source/ModularGameplayAbilities/Public/ModularAbilitySubsystem.h
@@ -0,0 +1,103 @@
+// Copyright © 2024 Playton. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ModularAbilitySystemComponent.h"
+#include "Subsystems/WorldSubsystem.h"
+
+#include "ModularAbilitySubsystem.generated.h"
+
+/** Struct containing all abilities that are applied to all actors. */
+USTRUCT()
+struct FGloballyAppliedAbilities
+{
+	GENERATED_BODY()
+
+public:
+	void AddToAbilitySystem(TSubclassOf<UGameplayAbility> Ability, UModularAbilitySystemComponent* AbilitySystem);
+	void RemoveFromAbilitySystem(UModularAbilitySystemComponent* AbilitySystem);
+	void RemoveFromAll();
+
+public:
+	UPROPERTY()
+	TMap<TObjectPtr<UModularAbilitySystemComponent>, FGameplayAbilitySpecHandle> Handles;
+};
+
+/** Struct containing all effects that are applied to all actors. */
+USTRUCT()
+struct FGloballyAppliedEffects
+{
+	GENERATED_BODY()
+
+public:
+	void AddToAbilitySystem(TSubclassOf<UGameplayEffect> Effect, UModularAbilitySystemComponent* AbilitySystem);
+	void RemoveFromAbilitySystem(UModularAbilitySystemComponent* AbilitySystem);
+	void RemoveFromAll();
+
+public:
+	UPROPERTY()
+	TMap<TObjectPtr<UModularAbilitySystemComponent>, FActiveGameplayEffectHandle> Handles;
+};
+
+/**
+ * Global subsystem for modular gameplay abilities.
+ * Manages granting and removing abilities from actors.
+ */
+UCLASS(Config = Game)
+class MODULARGAMEPLAYABILITIES_API UModularAbilitySubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	UModularAbilitySubsystem();
+
+	//~ Begin UWorldSubsystem Interface
+	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+	//~ End UWorldSubsystem Interface
+
+	/**
+	 * Registers an ability system component with the subsystem.
+	 *
+	 * @param AbilitySystem The ability system component to register.
+	 * @param bGrantPendingAbilities If true, abilities that were added to all actors before this ability system was registered will be granted.
+	 * @param bGrantPendingEffects If true, effects that were added to all actors before this ability system was registered will be granted.
+	 */
+	virtual void RegisterAbilitySystem(UModularAbilitySystemComponent* AbilitySystem, bool bGrantPendingAbilities = true, bool bGrantPendingEffects = true);
+
+	/** Unregisters an ability system component with the subsystem. */
+	virtual void UnregisterAbilitySystem(UModularAbilitySystemComponent* AbilitySystem);
+
+	/** Applies a gameplay ability to all actors. */
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Ability|Global")
+	void ApplyAbilityToAll(TSubclassOf<UGameplayAbility> Ability);
+
+	/** Applies a gameplay effect to all actors. */
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Ability|Global")
+	void ApplyEffectToAll(TSubclassOf<UGameplayEffect> Effect);
+
+	/** Removes a gameplay ability from all actors. */
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Ability|Global")
+	void RemoveAbilityFromAll(TSubclassOf<UGameplayAbility> Ability);
+
+	/** Removes a gameplay effect from all actors. */
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Ability|Global")
+	void RemoveEffectFromAll(TSubclassOf<UGameplayEffect> Effect);
+
+protected:
+	/** Gets the list of all registered ability system components. */
+	const TArray<TObjectPtr<UModularAbilitySystemComponent>>& GetRegisteredAbilitySystems() const { return RegisteredAbilitySystems; }
+
+private:
+	/** List of all globally applied abilities. */
+	UPROPERTY()
+	TMap<TSubclassOf<UGameplayAbility>, FGloballyAppliedAbilities> GloballyAppliedAbilities;
+
+	/** List of all globally applied effects. */
+	UPROPERTY()
+	TMap<TSubclassOf<UGameplayEffect>, FGloballyAppliedEffects> GloballyAppliedEffects;
+	
+	/** List of all registered ability system components. */
+	UPROPERTY()
+	TArray<TObjectPtr<UModularAbilitySystemComponent>> RegisteredAbilitySystems;
+};
