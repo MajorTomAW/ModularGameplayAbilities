67e6136dc2cf243d40731716a665ebf54dcba885
MajorTomAW <tom.wrnr.private@gmail.com>
2025-01-18 18:22:35 +0100

    BUGFIX: Class Filter for GameplayAbilities works now as intended

--- Source/ModularGameplayAbilitiesEditor/Private/AssetTools/GameplayAbilityFactory.cpp
+++ Source/ModularGameplayAbilitiesEditor/Private/AssetTools/GameplayAbilityFactory.cpp
@@ -7,6 +7,7 @@
 #include "KismetCompilerModule.h"
 #include "ModularGameplayAbilitiesClassFilter.h"
 #include "Abilities/GameplayAbility.h"
+#include "Kismet/BlueprintMapLibrary.h"
 #include "Kismet2/KismetEditorUtilities.h"
 #include "Kismet2/SClassPickerDialog.h"
 
@@ -24,13 +25,8 @@ UGameplayAbilityFactory::UGameplayAbilityFactory()
 
 	bCreateNew = true;
 	bEditAfterNew = true;
-	BlueprintType = BPTYPE_Normal;
 
 	CommonAbilityClasses.Add(UGameplayAbility::StaticClass());
-
-	TArray<UClass*> DerivedClasses;
-	GetDerivedClasses(UGameplayAbility::StaticClass(), DerivedClasses);
-	bSkipClassPicker = DerivedClasses.Num() == 0;
 }
 
 FText UGameplayAbilityFactory::GetDisplayName() const
@@ -42,7 +38,7 @@ bool UGameplayAbilityFactory::ConfigureProperties()
 {
 	TArray<UClass*> DerivedClasses;
 	GetDerivedClasses(ParentClass, DerivedClasses, false);
-	if (bSkipClassPicker || DerivedClasses.Num() == 0)
+	if (DerivedClasses.Num() == 0 || bSkipClassPicker)
 	{
 		// If there is only one derived class, skip the class picker
 		return true;
@@ -52,20 +48,24 @@ bool UGameplayAbilityFactory::ConfigureProperties()
 	ParentClass = nullptr;
 
 	// Fill in options
-	TSharedPtr<FModularGameplayAbilitiesClassFilter> Filter = MakeShareable(new FModularGameplayAbilitiesClassFilter(UGameplayAbility::StaticClass()));
 	FClassViewerInitializationOptions Options;
-	{
-		Options.Mode = EClassViewerMode::ClassPicker;
-		Options.NameTypeToDisplay = EClassViewerNameTypeToDisplay::Dynamic;
-		Options.bShowNoneOption = false;
-		Options.ClassFilters.Add(Filter.ToSharedRef());
-		Options.ExtraPickerCommonClasses = CommonAbilityClasses;
-		Options.bShowDefaultClasses = false;
-	}
+	Options.Mode = EClassViewerMode::ClassPicker;
+	Options.ExtraPickerCommonClasses.Empty();
+	Options.DisplayMode = EClassViewerDisplayMode::ListView;
+	Options.bAllowViewOptions = true;
+	Options.bExpandRootNodes = true;
+	Options.bShowUnloadedBlueprints = true;
+	Options.bShowObjectRootClass = true;
+
+	TArray<UClass*> CommonClasses = CommonAbilityClasses;
+	Options.ExtraPickerCommonClasses = CommonClasses;
+
+	TSharedPtr<FModularGameplayAbilitiesClassFilter> Filter = MakeShareable(new FModularGameplayAbilitiesClassFilter(UGameplayAbility::StaticClass()));
+	Options.ClassFilters.Add(Filter.ToSharedRef());
 
 	const FText TitleText = LOCTEXT("PickParentClass", "Pick Parent Class for New Gameplay Ability");
 	UClass* ChosenClass = nullptr;
-	const bool bPressedOk = SClassPickerDialog::PickClass(TitleText, Options, ChosenClass, UBlueprint::StaticClass());
+	const bool bPressedOk = SClassPickerDialog::PickClass(TitleText, Options, ChosenClass, UGameplayAbility::StaticClass());
 
 	if (bPressedOk)
 	{
@@ -93,7 +93,7 @@ UObject* UGameplayAbilityFactory::FactoryCreateNew(
 	KismetCompilerModule.GetBlueprintTypesForClass(ParentClass, BlueprintClass, BlueprintGeneratedClass);
 
 	UBlueprint* Blueprint =
-		FKismetEditorUtilities::CreateBlueprint(ParentClass, InParent, InName, BlueprintType, BlueprintClass, BlueprintGeneratedClass, CallingContext);
+		FKismetEditorUtilities::CreateBlueprint(ParentClass, InParent, InName, BPTYPE_Normal, BlueprintClass, BlueprintGeneratedClass, CallingContext);
 
 	// Mark the Blueprint as Data-Only
 	Blueprint->bRecompileOnLoad = false;
@@ -111,21 +111,8 @@ UGameplayEffectFactory::UGameplayEffectFactory()
 
 	bCreateNew = true;
 	bEditAfterNew = true;
-	BlueprintType = BPTYPE_Normal;
-	bSkipClassPicker = false;
 
-	OnConfigurePropertiesDelegate.BindLambda([&](FClassViewerInitializationOptions* Options)
-	{
-		Options->bShowNoneOption = false;
-		Options->ClassFilters.Reset();
-		Options->ClassFilters.Add(MakeShareable(new FModularGameplayAbilitiesClassFilter(UGameplayEffect::StaticClass())));
-		Options->ExtraPickerCommonClasses.Reset();
-		Options->ExtraPickerCommonClasses = CommonEffectClasses;
-		Options->bIsBlueprintBaseOnly = false;
-		Options->DisplayMode = EClassViewerDisplayMode::ListView;
-		Options->bAllowViewOptions = false;
-		Options->bShowDefaultClasses = false;
-	});
+	CommonEffectClasses.Add(UGameplayEffect::StaticClass());
 }
 
 FText UGameplayEffectFactory::GetDisplayName() const
@@ -135,7 +122,44 @@ FText UGameplayEffectFactory::GetDisplayName() const
 
 bool UGameplayEffectFactory::ConfigureProperties()
 {
-	return Super::ConfigureProperties();
+	TArray<UClass*> DerivedClasses;
+	GetDerivedClasses(ParentClass, DerivedClasses, false);
+	if (DerivedClasses.Num() == 0 || bSkipClassPicker)
+	{
+		// If there is only one derived class, skip the class picker
+		return true;
+	}
+	
+	// Null the parent class to ensure one is selected
+	ParentClass = nullptr;
+
+	// Fill in options
+	FClassViewerInitializationOptions Options;
+	Options.Mode = EClassViewerMode::ClassPicker;
+	Options.ExtraPickerCommonClasses.Empty();
+	Options.DisplayMode = EClassViewerDisplayMode::ListView;
+	Options.bAllowViewOptions = true;
+	Options.bExpandRootNodes = true;
+	Options.bShowUnloadedBlueprints = true;
+	Options.bShowObjectRootClass = true;
+
+	TArray<UClass*> CommonClasses = CommonEffectClasses;
+	Options.ExtraPickerCommonClasses = CommonClasses;
+
+	TSharedPtr<FModularGameplayAbilitiesClassFilter> Filter = MakeShareable(new FModularGameplayAbilitiesClassFilter(UGameplayEffect::StaticClass()));
+	Filter->DisallowedChildrenOfClasses.Add(UBlueprintMapLibrary::StaticClass());
+	Options.ClassFilters.Add(Filter.ToSharedRef());
+
+	const FText TitleText = LOCTEXT("PickParentClass", "Pick Parent Class for New Gameplay Ability");
+	UClass* ChosenClass = nullptr;
+	const bool bPressedOk = SClassPickerDialog::PickClass(TitleText, Options, ChosenClass, UGameplayEffect::StaticClass());
+
+	if (bPressedOk)
+	{
+		ParentClass = ChosenClass;
+	}
+
+	return bPressedOk;
 }
 
 UObject* UGameplayEffectFactory::FactoryCreateNew(
@@ -156,7 +180,7 @@ UObject* UGameplayEffectFactory::FactoryCreateNew(
 	KismetCompilerModule.GetBlueprintTypesForClass(ParentClass, BlueprintClass, BlueprintGeneratedClass);
 
 	UBlueprint* Blueprint =
-		FKismetEditorUtilities::CreateBlueprint(ParentClass, InParent, InName, BlueprintType, BlueprintClass, BlueprintGeneratedClass, CallingContext);
+		FKismetEditorUtilities::CreateBlueprint(ParentClass, InParent, InName, BPTYPE_Normal, BlueprintClass, BlueprintGeneratedClass, CallingContext);
 
 	// Mark the Blueprint as Data-Only
 	Blueprint->bRecompileOnLoad = false;
--- Source/ModularGameplayAbilitiesEditor/Private/AssetTools/ModularGameplayAbilitiesClassFilter.h
+++ Source/ModularGameplayAbilitiesEditor/Private/AssetTools/ModularGameplayAbilitiesClassFilter.h
@@ -8,12 +8,12 @@ class FModularGameplayAbilitiesClassFilter : public IClassViewerFilter
 {
 public:
 	FModularGameplayAbilitiesClassFilter()
-		: DisallowedClassFlags(CLASS_Abstract | CLASS_Deprecated | CLASS_NewerVersionExists | CLASS_HideDropDown)
+		: DisallowedClassFlags(CLASS_Deprecated | CLASS_NewerVersionExists | CLASS_HideDropDown)
 	{
 	}
 
 	explicit FModularGameplayAbilitiesClassFilter(const UClass* AllowedClass)
-		: DisallowedClassFlags(CLASS_Abstract | CLASS_Deprecated | CLASS_NewerVersionExists | CLASS_HideDropDown)
+		: DisallowedClassFlags(CLASS_Deprecated | CLASS_NewerVersionExists | CLASS_HideDropDown)
 	{
 		AllowedChildrenOfClasses.Add(AllowedClass);
 		ParentClass = AllowedClass;
@@ -21,21 +21,26 @@ public:
 	
 	const UClass* ParentClass = nullptr;
 	TSet<const UClass*> AllowedChildrenOfClasses;
+	TSet<const UClass*> DisallowedChildrenOfClasses;
 	EClassFlags DisallowedClassFlags;
 
 public:
 	virtual bool IsClassAllowed(const FClassViewerInitializationOptions& InInitOptions, const UClass* InClass, TSharedRef<class FClassViewerFilterFuncs> InFilterFuncs) override
 	{
-		return InClass &&
+		const bool bIsClassAllowed = InClass &&
 			!InClass->HasAnyClassFlags(DisallowedClassFlags) &&
-			( InFilterFuncs->IfInChildOfClassesSet(AllowedChildrenOfClasses, InClass) != EFilterReturn::Failed ||
-				InClass->IsChildOf(ParentClass));
+				( InFilterFuncs->IfInChildOfClassesSet(AllowedChildrenOfClasses, InClass) != EFilterReturn::Failed) ||
+					InClass->IsChildOf(ParentClass) &&
+				( InFilterFuncs->IfInChildOfClassesSet(DisallowedChildrenOfClasses, InClass) != EFilterReturn::Passed );
+		
+		return bIsClassAllowed;
 	}
 
 	virtual bool IsUnloadedClassAllowed(const FClassViewerInitializationOptions& InInitOptions, const TSharedRef<const class IUnloadedBlueprintData> InUnloadedClassData, TSharedRef<class FClassViewerFilterFuncs> InFilterFuncs) override
 	{
 		return !InUnloadedClassData->HasAnyClassFlags(DisallowedClassFlags) &&
 			( InFilterFuncs->IfInChildOfClassesSet(AllowedChildrenOfClasses, InUnloadedClassData) != EFilterReturn::Failed ||
-				InUnloadedClassData->IsChildOf(ParentClass));
+				InUnloadedClassData->IsChildOf(ParentClass)) &&
+			( InFilterFuncs->IfInChildOfClassesSet(DisallowedChildrenOfClasses, InUnloadedClassData) != EFilterReturn::Passed );
 	}
 };
