601c66588440ca801cbfd6ba99ed8090d2f4b6ab
MajorTomAW <tom.wrnr.private@gmail.com>
2026-02-27 18:11:28 +0100

    added a WaitForEnhanccedInput ability task

---
+++ Source/ModularGameplayAbilities/Private/Tasks/AbilityTask_WaitForEnhancedInput.cpp
@@ -0,0 +1,130 @@
+﻿// Author: Tom Werner (dc: majort), 2026 January
+
+
+#include "Tasks/AbilityTask_WaitForEnhancedInput.h"
+
+#include "EnhancedInputComponent.h"
+
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(AbilityTask_WaitForEnhancedInput)
+
+UAbilityTask_WaitForEnhancedInput::UAbilityTask_WaitForEnhancedInput(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+UAbilityTask_WaitForEnhancedInput* UAbilityTask_WaitForEnhancedInput::WaitForEnhancedInput(
+	UGameplayAbility* OwningAbility,
+	FName TaskInstanceName,
+	UInputAction* InputAction,
+	bool bOnePressEventOnly)
+{
+	UAbilityTask_WaitForEnhancedInput* NewTask = NewAbilityTask<ThisClass>(OwningAbility, TaskInstanceName);
+	NewTask->InputAction = InputAction;
+	NewTask->bOnePressEventOnly = bOnePressEventOnly;
+	return NewTask;
+}
+
+void UAbilityTask_WaitForEnhancedInput::Activate()
+{
+	Super::Activate();
+
+	if (!IsValid(Ability) || !AbilitySystemComponent.IsValid() || !InputAction.IsValid())
+	{
+		return;
+	}
+
+	const APlayerController* PC = Ability->GetActorInfo().PlayerController.Get();
+	if (!IsValid(PC))
+	{
+		return;
+	}
+
+	EnhancedInput = Cast<UEnhancedInputComponent>(PC->InputComponent);
+	if (EnhancedInput.IsValid())
+	{
+		EnhancedInput->BindAction(InputAction.Get(), ETriggerEvent::Started, this, &ThisClass::HandleInputStarted);
+		EnhancedInput->BindAction(InputAction.Get(), ETriggerEvent::Ongoing, this, &ThisClass::HandleInputOngoing);
+		EnhancedInput->BindAction(InputAction.Get(), ETriggerEvent::Triggered, this, &ThisClass::HandleInputTriggered);
+		EnhancedInput->BindAction(InputAction.Get(), ETriggerEvent::Canceled, this, &ThisClass::HandleInputCanceled);
+		EnhancedInput->BindAction(InputAction.Get(), ETriggerEvent::Completed, this, &ThisClass::HandleInputCompleted);
+	}
+}
+
+void UAbilityTask_WaitForEnhancedInput::ExternalCancel()
+{
+	Super::ExternalCancel();
+}
+
+FString UAbilityTask_WaitForEnhancedInput::GetDebugString() const
+{
+	return Super::GetDebugString();
+}
+
+void UAbilityTask_WaitForEnhancedInput::OnDestroy(bool bInOwnerFinished)
+{
+	if (EnhancedInput.IsValid())
+	{
+		EnhancedInput->ClearBindingsForObject(this);
+	}
+
+	Super::OnDestroy(bInOwnerFinished);
+}
+
+void UAbilityTask_WaitForEnhancedInput::HandleInputStarted(const FInputActionValue& ActionValue)
+{
+	if (bOnePressEventOnly && bHasBeenTriggered)
+	{
+		return;
+	}
+	bHasBeenTriggered = true;
+	OnInputStarted.Broadcast(ActionValue);
+}
+
+void UAbilityTask_WaitForEnhancedInput::HandleInputOngoing(const FInputActionValue& ActionValue)
+{
+	if (bOnePressEventOnly && bHasBeenTriggered)
+	{
+		return;
+	}
+	bHasBeenTriggered = true;
+	OnInputOngoing.Broadcast(ActionValue);
+}
+
+void UAbilityTask_WaitForEnhancedInput::HandleInputTriggered(const FInputActionValue& ActionValue)
+{
+	if (bOnePressEventOnly && bHasBeenTriggered)
+	{
+		return;
+	}
+
+	// Not triggered yet? -> so its the first time
+	if (!bHasBeenTriggered)
+	{
+		OnInputFirstTriggered.Broadcast(ActionValue);
+		return;
+	}
+
+	bHasBeenTriggered = true;
+	OnInputTriggered.Broadcast(ActionValue);
+}
+
+void UAbilityTask_WaitForEnhancedInput::HandleInputCanceled(const FInputActionValue& ActionValue)
+{
+	if (bOnePressEventOnly && bHasBeenTriggered)
+	{
+		return;
+	}
+	bHasBeenTriggered = true;
+	OnInputCanceled.Broadcast(ActionValue);
+}
+
+void UAbilityTask_WaitForEnhancedInput::HandleInputCompleted(const FInputActionValue& ActionValue)
+{
+	if (bOnePressEventOnly && bHasBeenTriggered)
+	{
+		return;
+	}
+	bHasBeenTriggered = true;
+	OnInputCompleted.Broadcast(ActionValue);
+}
---
+++ Source/ModularGameplayAbilities/Public/Tasks/AbilityTask_WaitForEnhancedInput.h
@@ -0,0 +1,71 @@
+﻿// Author: Tom Werner (dc: majort), 2026 January
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputActionValue.h"
+#include "Abilities/Tasks/AbilityTask.h"
+
+#include "AbilityTask_WaitForEnhancedInput.generated.h"
+
+class UObject;
+class UInputAction;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FEnhancedInputEventDelegate, FInputActionValue, ActionValue);
+
+/** Ability task that waits for an enhanced input event. */
+UCLASS(MinimalAPI)
+class UAbilityTask_WaitForEnhancedInput : public UAbilityTask
+{
+	GENERATED_BODY()
+
+public:
+	UAbilityTask_WaitForEnhancedInput(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	UPROPERTY(BlueprintAssignable)
+	FEnhancedInputEventDelegate OnInputStarted;
+
+	UPROPERTY(BlueprintAssignable)
+	FEnhancedInputEventDelegate OnInputOngoing;
+
+	UPROPERTY(BlueprintAssignable)
+	FEnhancedInputEventDelegate OnInputFirstTriggered;
+
+	UPROPERTY(BlueprintAssignable)
+	FEnhancedInputEventDelegate OnInputTriggered;
+
+	UPROPERTY(BlueprintAssignable)
+	FEnhancedInputEventDelegate OnInputCanceled;
+
+	UPROPERTY(BlueprintAssignable)
+	FEnhancedInputEventDelegate OnInputCompleted;
+
+	/**
+	 * Starts waiting for any enhanced input action events using the specified input action.
+	 * @param bOnePressEventOnly If true, will only listen for the first input event and then close the gate
+	 */
+	UFUNCTION(BlueprintCallable, Category="Ability|Tasks", meta=(HidePin=OwningAbility,DefaultToSelf=OwningAbility,BlueprintInternalUseOnly=true))
+	static UAbilityTask_WaitForEnhancedInput* WaitForEnhancedInput(UGameplayAbility* OwningAbility, FName TaskInstanceName, UInputAction* InputAction, bool bOnePressEventOnly);
+
+protected:
+	//~ Begin UAbilityTask Interface
+	virtual void Activate() override;
+	virtual void ExternalCancel() override;
+	virtual FString GetDebugString() const override;
+	virtual void OnDestroy(bool bInOwnerFinished) override;
+	//~ End UAbilityTask Interface
+
+	/** Bind Action handlers. */
+	void HandleInputStarted(const FInputActionValue& ActionValue);
+	void HandleInputOngoing(const FInputActionValue& ActionValue);
+	void HandleInputTriggered(const FInputActionValue& ActionValue);
+	void HandleInputCanceled(const FInputActionValue& ActionValue);
+	void HandleInputCompleted(const FInputActionValue& ActionValue);
+
+private:
+	bool bOnePressEventOnly;
+
+	TWeakObjectPtr<UInputAction> InputAction;
+	TWeakObjectPtr<UEnhancedInputComponent> EnhancedInput;
+	bool bHasBeenTriggered = false;
+};
